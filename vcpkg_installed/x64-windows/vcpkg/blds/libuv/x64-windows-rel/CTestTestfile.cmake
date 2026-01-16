55ffe39d97250e18faecc9c9bbf47030b198cea6
Dan <silversteindaniel6@gmail.com>
2026-01-15 22:31:03 -0500

    fucking visual studio

--- vcpkg_installed/x64-windows/vcpkg/blds/libuv/x64-windows-rel/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/dev/P4AUxSAMMI/vcpkg_installed/x64-windows/vcpkg/blds/libuv/src/v1.51.0-0362e46844.clean
-# Build directory: D:/dev/P4AUxSAMMI/vcpkg_installed/x64-windows/vcpkg/blds/libuv/x64-windows-rel
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
